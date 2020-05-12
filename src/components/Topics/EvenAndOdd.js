312dbbdb9c0628c6625d3d862912eff8eeef9dd2
Eliottnibley <e.nibley@gmail.com>
2020-05-12 14:20:32 -0600

    update

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,9 +2,44 @@ import React from 'react'
 import {Component} from 'react'
 
 export default class EvenAndOdd extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: ''
+    }
+  }
+
+  change(val) {
+    this.setState({userInput: val})
+  }
+
+  solve(userInput) {
+    let array = userInput.split(',')
+    let even = []
+    let odd = []
+    for (let i = 0; i< array.length; i++){
+      if (array[i] % 2 === 0){
+        even.push(parseInt(array[i], 10))
+      }
+      else {
+        odd.push(parseInt(array[i], 10))
+      }
+    }
+    this.setState({evenArray: even, oddArray: odd})
+  }
+
   render() {
     return(
-      <p>EvenAndOdd</p>
+      <div className='evenAndOddPB'>
+        <h4>Evens and Odds</h4>
+        <input onChange= {(e) => this.change(e.target.value)} className='inputLine'/>
+        <button onClick = {this.solve(this.state.userInput)} className='confirmationButton'>Split</button>
+        <span className='resultsBox'></span>
+        <span className='resultsBox'></span>
+      </div>
     )
   }
 }
